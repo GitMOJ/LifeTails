b423bc4636470a666e9e4f9a3cc138f1661caa22
Muhammad Osama Javaid <87387879+GitMOJ@users.noreply.github.com>
2021-09-27 17:32:47 +0500

    footer remaining

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,20 @@
-$(document).ready(function(){
-    $nav = $('nav');
-    $toggleCollapse = $('.toggle-collapse');
+// $(document).ready(function(){
+//     $nav = $('nav');
+//     $toggleCollapse = $('.toggle-collapse');
 
-    $toggleCollapse.click(function(){
-        $nav.$toggleCollapse('collapse');
-    })
+//     $toggleCollapse.click(function(){
+//         $nav.$toggleCollapse('collapse');
+//     })
 
-    // owl carousel
-    $(".owl-carousel").owlCarousel();
+//     // owl carousel
+//     $(".owl-carousel").owlCarousel();
+// });
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    dotsEach: true,
+    // lazyload: true
+    
 });
